cd02fb7442cb23d9b02f223fcbfec288168ed2d8
Mrt <mertkonrat89@gmail.com>
2023-02-26 14:11:31 +0300

    update

--- src/components/editor/Editor1.js
+++ src/components/editor/Editor1.js
@@ -1,14 +1,19 @@
-import Split from 'react-split';
+import Split from "react-split";
 
 export const Editor1 = () => {
   return (
-    <Split direction='vertical' style={{ height: `calc(100vh - 4rem)` }}>
-      <Split className='flex'>
-        <div className='bg-gray-300'></div>
-        <div className='bg-gray-300'></div>
-        <div className='bg-gray-300'></div>
+    <Split direction="vertical" style={{ height: "calc(100vh)" }}>
+      <Split className="d-flex" direction="horizontal">
+        <div
+          className="bg-success"
+          style={{ width: "100%", height: "50%" }}
+        ></div>
+        <div
+          className="bg-danger"
+          style={{ width: "100%", height: "50%" }}
+        ></div>
       </Split>
-      <div className='bg-gray-400'></div>
+      <div className="bg-info"></div>
     </Split>
   );
 };
