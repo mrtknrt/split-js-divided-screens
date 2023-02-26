681d3d75cbb7d5fc79051cb96a39fd662cbd375a
Mrt <mertkonrat89@gmail.com>
2023-02-26 14:32:40 +0300

    update

--- src/components/editor/Editor1.js
+++ src/components/editor/Editor1.js
@@ -4,16 +4,13 @@ export const Editor1 = () => {
   return (
     <Split direction="vertical" style={{ height: "calc(100vh)" }}>
       <Split className="d-flex" direction="horizontal">
-        <div
-          className="bg-success"
-          style={{ width: "100%", height: "50%" }}
-        ></div>
-        <div
-          className="bg-danger"
-          style={{ width: "100%", height: "50%" }}
-        ></div>
+        <div className="bg-success" style={{ width: "100%" }}></div>
+        <div className="bg-danger" style={{ width: "100%" }}></div>
       </Split>
-      <div className="bg-info"></div>
+      <Split className="d-flex" direction="horizontal">
+        <div className="bg-success" style={{ width: "100%" }}></div>
+        <div className="bg-danger" style={{ width: "100%" }}></div>
+      </Split>
     </Split>
   );
 };
