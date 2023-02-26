9daa043ceb7200635da92b86b57c54145b2093c5
Mrt <mertkonrat89@gmail.com>
2023-02-26 16:02:51 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Split from "react-split";
 import TableTop from "./components/TableTop";
 import Coordinates from "./components/Coordinates";
@@ -6,13 +6,32 @@ import TableBottom from "./components/TableBottom";
 import TextArea from "./components/TextArea";
 
 export default function MyApp(Component, pageProps) {
+  const [topWidth, setTopWidth] = useState(0);
+  const [topHeight, setTopHeight] = useState(0);
+  const [bottomWidth, setBottomWidth] = useState(0);
+  const [bottomHeight, setBottomHeight] = useState(0);
+
   return (
     <Split direction="vertical" style={{ height: "calc(100vh)" }}>
-      <Split className="d-flex" direction="horizontal">
+      <Split
+        className="d-flex"
+        direction="horizontal"
+        onDragEnd={(sizes) => {
+          setTopWidth(sizes[0]);
+          setTopHeight(sizes[1]);
+        }}
+      >
         <TableTop />
-        <Coordinates />
+        <Coordinates width={topWidth} height={topHeight} />
       </Split>
-      <Split className="d-flex" direction="horizontal">
+      <Split
+        className="d-flex"
+        direction="horizontal"
+        onDragEnd={(sizes) => {
+          setBottomWidth(sizes[0]);
+          setBottomHeight(sizes[1]);
+        }}
+      >
         <TableBottom />
         <TextArea />
       </Split>
--- src/components/Coordinates.js
+++ src/components/Coordinates.js
@@ -1,7 +1,19 @@
 import React from "react";
 
-const Coordinates = () => {
-  return <div style={{ width: "100%" }}></div>;
+const Coordinates = ({ width, height }) => {
+  return (
+    <div style={{ width: "100%" }}>
+      <div className="container">
+        <div className="row justify-content-end">
+          <div className="col-6 text-right backgroundColor">
+            <div className="fw-bold">Ayarlar</div>
+            <div> Top Width: {width} </div> <br />
+            <div> Top Height: {height}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Coordinates;
