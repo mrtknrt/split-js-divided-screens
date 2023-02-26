b94d94555727367ae41557818a98e63b5550c872
Mrt <mertkonrat89@gmail.com>
2023-02-26 14:50:47 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
-import { Layout, Editor1 } from "./components";
+import Split from "react-split";
 
 export default function MyApp(Component, pageProps) {
   return (
-    <Layout {...pageProps}>
-      <Editor1 />
-    </Layout>
+    <Split direction="vertical" style={{ height: "calc(100vh)" }}>
+      <Split className="d-flex" direction="horizontal">
+        <div className="bg-success" style={{ width: "100%" }}></div>
+        <div className="bg-danger" style={{ width: "100%" }}></div>
+      </Split>
+      <Split className="d-flex" direction="horizontal">
+        <div className="bg-success" style={{ width: "100%" }}></div>
+        <div className="bg-danger" style={{ width: "100%" }}></div>
+      </Split>
+    </Split>
   );
 }
