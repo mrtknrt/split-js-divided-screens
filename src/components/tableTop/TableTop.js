10dee14542604e45a565eb8f7a6a3a9be1ebe677
Mrt <mertkonrat89@gmail.com>
2023-02-27 21:08:54 +0300

    update

--- src/components/tableTop/TableTop.js
+++ src/components/tableTop/TableTop.js
@@ -1,7 +1,86 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const TableTop = () => {
-  return <div style={{ width: "100%" }}></div>;
-};
+function PostsTable() {
+  const [posts, setPosts] = useState([]);
+  const [selectedYear, setSelectedYear] = useState("all");
+  const [showColumns, setShowColumns] = useState({
+    id: true,
+    title: true,
+    body: true,
+  });
 
-export default TableTop;
+  useEffect(() => {
+    let apiUrl = "https://jsonplaceholder.typicode.com/posts?_limit=5";
+
+    if (selectedYear !== "all") {
+      apiUrl += `?title=${selectedYear}`;
+    }
+
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => setPosts(data))
+      .catch((error) => console.log(error));
+  }, [selectedYear, showColumns]);
+
+  const handleYearChange = (e) => {
+    setSelectedYear(e.target.value);
+  };
+
+  const handleColumnToggle = (col) => {
+    setShowColumns((prevColumns) => ({
+      ...prevColumns,
+      [col]: !prevColumns[col],
+    }));
+  };
+
+  const tableHeaders = Object.keys(showColumns)
+    .filter((col) => showColumns[col])
+    .map((col) => <th key={col}>{col}</th>);
+
+  const tableRows = posts.map((post) => {
+    const rowData = Object.keys(showColumns)
+      .filter((col) => showColumns[col])
+      .map((col) => <td key={col}>{post[col]}</td>);
+
+    return <tr key={post.id}>{rowData}</tr>;
+  });
+
+  return (
+    <div>
+      <div className="filters">
+        <div className="dropdown">
+          <label htmlFor="year-filter">Filter by year:</label>
+          <select
+            id="year-filter"
+            value={selectedYear}
+            onChange={handleYearChange}
+          >
+            <option value="all">All</option>
+            <option value="2018">2018</option>
+            <option value="2019">2019</option>
+          </select>
+        </div>
+        <div className="checkboxes">
+          {Object.keys(showColumns).map((col) => (
+            <label key={col}>
+              <input
+                type="checkbox"
+                checked={showColumns[col]}
+                onChange={() => handleColumnToggle(col)}
+              />
+              {col}
+            </label>
+          ))}
+        </div>
+      </div>
+      <table>
+        <thead>
+          <tr>{tableHeaders}</tr>
+        </thead>
+        <tbody>{tableRows}</tbody>
+      </table>
+    </div>
+  );
+}
+
+export default PostsTable;
